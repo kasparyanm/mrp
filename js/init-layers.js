1db69fdd5a366348f7926f9e011122b00866ca17
kasparyanm <kasparyanm@gmail.com>
2017-12-23 19:51:23 +0300

    'fix'

--- js/init-layers.js
+++ js/init-layers.js
@@ -12,18 +12,34 @@ function initLayers(){
 		}),
 	    style: styleFunction
 	});
-
-	layerProm = new ol.layer.Vector({
-	    'title': 'Промышленность',
-	    source: new ol.source.Vector({
+	var promSource= new ol.source.Vector({
 		    format: new ol.format.GeoJSON(),
-		    url: function(extent) {
-		      return 'https://gs.gismart.ru/geoserver2/mainMap/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=mainMap:product&maxFeatures=500&' +
+		    loader: function(extent, resolution, projection) {
+			    var url = 'https://gs.gismart.ru/geoserver2/mainMap/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=mainMap:product&maxFeatures=500&' +
 		          'outputFormat=application/json&srsname=EPSG:3857&' +
 		          'bbox=' + extent.join(',')+',EPSG:3857';
-		    },
+			    var xhr = new XMLHttpRequest();
+			    xhr.open('GET', url);
+			    var onError = function() {
+			    	promSource.removeLoadedExtent(extent);
+			    }
+			    xhr.onerror = onError;
+			    xhr.onload = function() {
+			    	if (xhr.status == 200) {
+			    		promSource.clear();
+			    		promSource.addFeatures(promSource.getFormat().readFeatures(xhr.responseText));
+			    	} else {
+			    		onError();
+			   		}
+				}
+				xhr.send();
+			},
+		    
 		    strategy: ol.loadingstrategy.bbox
-		}),
+		});
+	layerProm = new ol.layer.Vector({
+	    'title': 'Промышленность',
+	    source: promSource,
 	    style: styleFunction
 	});
 	
@@ -80,18 +96,33 @@ function initLayers(){
 	    }),
 	    style: styleFunction
 	})
-
+	var vectorForInvestClust = new ol.source.Vector({
+		    format: new ol.format.GeoJSON(),
+		    loader: function(extent, resolution, projection) {
+			    var url = 'https://gs.gismart.ru/geoserver2/mainMap/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=mainMap:invest&maxFeatures=500&' +
+		          'outputFormat=application/json&srsname=EPSG:3857&' +
+		          'bbox=' + extent.join(',')+',EPSG:3857'
+			    var xhr = new XMLHttpRequest();
+			    xhr.open('GET', url);
+			    var onError = function() {
+			    	vectorForInvestClust.removeLoadedExtent(extent);
+			    }
+			    xhr.onerror = onError;
+			    xhr.onload = function() {
+			    	if (xhr.status == 200) {
+			    		vectorForInvestClust.clear();
+			    		vectorForInvestClust.addFeatures(vectorForInvestClust.getFormat().readFeatures(xhr.responseText));
+			    	} else {
+			    		onError();
+			   		}
+				}
+				xhr.send();
+			},
+		    strategy: ol.loadingstrategy.bbox
+		});
 	var investClusterSource = new ol.source.Cluster({
 		distance: 46,
-		source: new ol.source.Vector({
-		    format: new ol.format.GeoJSON(),
-		    url: function(extent) {
-		      return 'https://gs.gismart.ru/geoserver2/mainMap/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=mainMap:invest&maxFeatures=500&' +
-		          'outputFormat=application/json&srsname=EPSG:3857&' +
-		          'bbox=' + extent.join(',')+',EPSG:3857';
-		    },
-		    strategy: ol.loadingstrategy.bbox
-		})
+		source: vectorForInvestClust
 	});
 
 	var styleCache = {};
